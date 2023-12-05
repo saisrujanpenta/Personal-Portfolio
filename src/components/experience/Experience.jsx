8323120495af5739fb9d25ecc1cda392ed36358e
saisrujanpenta <penta.s@northeastern.edu>
2023-12-05 15:26:27 -0500

    updated resume and experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -26,17 +26,17 @@ const Experience = () => {
             <ul className='experience_details-content'>
               <li>
                 <BiCheck className='experience_details-icon' />
-                <p>Constructed reliable solutions using JavaScript and React.js design pattern to meet client demands for functionality, scalability and performance attained an average of 95% client satisfaction ratings</p>
+                <p>Spearheaded initiatives to boost system performance by implementing cutting-edge monitoring dashboards and fine-tuning performance tracking tools, resulting in a remarkable 25% reduction in issue resolution time</p>
               </li>
               <li><BiCheck className='experience_details-icon' />
+                <p>Pioneered implementation of streamlined processes for problem resolution and decision-making by introducing monitoring dashboards on Fiori Launchpad, leading to an impressive 50% reduction in issue identification</p>
+              </li>
+              <li><BiCheck className='experience_details-icon' />
+                <p>Played a pivotal role in training and empowering a functional team on platform utilization, significantly enhancing team proficiency and overall productivity</p>
+              </li>
+              {/* <li><BiCheck className='experience_details-icon' />
                 <p>Designed web-based software using CSS, HTML, JavaScript and Web API to improve user experience and accomplished a 20% reduction in development time through use of templates and table-driven methods</p>
-              </li>
-              <li><BiCheck className='experience_details-icon' />
-                <p>Initiated and led training three peers in web technologies (HTML, CSS, JavaScript), ensuring responsive website creation for enhanced user satisfaction and leading to promotions</p>
-              </li>
-              <li><BiCheck className='experience_details-icon' />
-                <p>Coordinated with IT, business, program management teams in deploying project specific solutions</p>
-              </li>
+              </li> */}
             </ul>
           </div>
         </div>
@@ -55,11 +55,17 @@ const Experience = () => {
 
             <ul className='experience_details-content'>
               <li><BiCheck className='experience_details-icon2' />
-                <p>Executed HTML, CSS, SASS, and JavaScript to build and deliver responsive websites, demonstrating a seamless user experience and strict adherence to modern web development practices</p>
+                <p>Proactively monitored and managed system performance, generating insightful reports for relevant teams, contributing to a substantial improvement in system uptime and a 15% reduction in critical incidents</p>
+              </li>
+              <li><BiCheck className='experience_details-icon2' />
+                <p>Led configuration of a transport management system between landscapes, ensuring seamless transport of requests from development to quality and production environments, streamlining development lifecycle</p>
               </li>
               <li><BiCheck className='experience_details-icon2' />
                 <p>Achieved a track record of skillfully collaborating with diverse team members, clients and stakeholders, facilitating clear and concise information exchange for achieving favorable project outcomes</p>
               </li>
+              {/* <li><BiCheck className='experience_details-icon2' />
+                <p>Developed and maintained multiple responsive web applications using React, incorporating Material-UI for interactive user interfaces, resulting in a 20% increase in user engagement</p>
+              </li> */}
               {/* <li><BiCheck className='experience_details-icon2' />
                 <p>Established and verified Remote Function Call connections between systems to facilitate communication and data exchange</p>
               </li> */}
